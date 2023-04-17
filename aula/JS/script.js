d71ca5c3abe8b2fe51b64898de95025257b598da
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-04-17 20:00:00 -0300

    Add files via upload

---
+++ aula/JS/script.js
@@ -0,0 +1,9 @@
+/*
+//Declarando Variáveis no JS
+var nome='Eloá'
+let idade='16'
+document.write(nome + idade)
+*/
+
+let msg=prompt("Digite o seu nome")
+document.write("O valor digitado foi:  ", msg)
---
+++ aula/commit-m/README.md
@@ -0,0 +1,2 @@
+# AulaJS
+ AulaJS
