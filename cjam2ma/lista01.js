0597c9d0dd1c3e540cc1898cfe88f480844238c5
EloáVM22 <127448572+EloaVasconcelos@users.noreply.github.com>
2023-10-13 14:06:10 -0300

    Add files via upload

---
+++ cjam2ma/lista01.js
@@ -0,0 +1,22 @@
+
+// Exe 1
+const myBirthYear=2006
+console.log(myBirthYear)
+
+// Exe 2
+const currentYear=2023
+console.log(currentYear)
+
+// Exe3 - Print
+
+
+// Exe4 
+const myAge =(currentYear - myBirthYear)
+console.log(myAge)
+
+// Exe 5
+const ten =10
+console.log(`Em ${currentYear + ten} estarei com ${myAge + ten} anos`);
+
+
+
